301dbb4bceace673c0f84894ad59b35afa80dac2
Bass97345 <104982540+Bass97345@users.noreply.github.com>
2025-11-01 12:34:49 +0300

    Add files via upload

---
+++ Musick/Musick/Models/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Musick.Models;
+
+public partial class Tag
+{
+    public int TagId { get; set; }
+
+    public string NameTag { get; set; } = null!;
+
+    public virtual ICollection<Playlist> Playlists { get; set; } = new List<Playlist>();
+}
---
+++ Musick/Musick/Models/Track.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Musick.Models;
+
+public partial class Track
+{
+    public int TrackId { get; set; }
+
+    public string TrackName { get; set; } = null!;
+
+    public int Duration { get; set; }
+
+    public DateOnly RealiseDate { get; set; }
+
+    public int Bitrate { get; set; }
+
+    public string FilePath { get; set; } = null!;
+
+    public decimal Rating { get; set; }
+
+    public int? PlayCount { get; set; }
+
+    public virtual ICollection<AlbumsTreck> AlbumsTrecks { get; set; } = new List<AlbumsTreck>();
+
+    public virtual ICollection<Artist> Artists { get; set; } = new List<Artist>();
+
+    public virtual ICollection<Genre> Genres { get; set; } = new List<Genre>();
+
+    public virtual ICollection<Playlist> Playlists { get; set; } = new List<Playlist>();
+}
