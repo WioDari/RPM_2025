2f02127ed01ea6aa128f2a31ed616594f62b7a52
Bass97345 <104982540+Bass97345@users.noreply.github.com>
2025-11-27 12:19:33 +0300

    Add files via upload

---
+++ Musick/Musick/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Musick.Models;
+
+public partial class Genre
+{
+    public int GenresId { get; set; }
+
+    public string GenresName { get; set; } = null!;
+
+    public virtual ICollection<Album> Albums { get; set; } = new List<Album>();
+
+    public virtual ICollection<Artist> Artists { get; set; } = new List<Artist>();
+
+    public virtual ICollection<Track> Tracks { get; set; } = new List<Track>();
+}
