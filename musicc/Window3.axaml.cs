8d946a10b7c35b0094c64818bc1efdbbe40eabbf
sekibanki <qweh15@mail.ru>
2025-12-18 15:36:12 +0300

    что нибудь

---
+++ musicc/Window3.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace musicc;
+
+public partial class Window3 : Window
+{
+    public Window3()
+    {
+        InitializeComponent();
+    }
+}
